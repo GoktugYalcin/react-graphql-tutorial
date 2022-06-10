70f18d17e8e6bacf2e18337eb8303f46dee7ec62
A. Göktuğ Yalçın <goktug.yalcin@next4biz.com>
2022-06-10 16:41:10 +0300

    Animation added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
+import MissionCard from "./MissionCard";
 import './App.css';
 import { useQuery, gql } from "@apollo/client";
-import moment from 'moment';
 
 const FILMS_QUERY = gql`
  {
@@ -36,22 +36,12 @@ function App() {
 
   if(loading) return <div className="loading">Fetching Data...</div>
   if(error) return <div className="error">{error.message}</div>
-  if(data) {
-    console.log(data)
-  }
+
   return (
-    <div className="container">
-      {data.launchesPast.map((launch) => (<div className="mission" key={launch.id}>
-        <div className="mission_name" key={launch.id}>
-          <a href={launch.links.video_link} target="_blank" rel="noreferrer"><b>Mission Name:</b> {launch.mission_name}</a>
-          <p className="rocket_name"><b>Rocket Name:</b> {launch.rocket.rocket_name}</p>
-          <p className="mission_date"><b>Launch Date: </b> {moment(launch.rocket.launch_date_local).format("DD MMM YYYY hh:MM")}</p>
-          <p className="mission_place"><b>Launch Site: </b> {launch.launch_site.site_name_long}</p>
-          { launch.ships.length ? <div className="image">
-            <img width="256" height="256" src={launch.ships[0].image} />
-          </div> : <></> }
-        </div>
-      </div>))}
+    <div id="container">
+        {data.launchesPast.map((launch) => (
+            <MissionCard launch={launch} />
+        ))}
     </div>
   );
 }
---
+++ src/MissionCard.js
@@ -0,0 +1,24 @@
+import React from "react";
+import moment from 'moment';
+import Fade from "react-reveal/Fade";
+
+const MissionCard = ({launch}) => {
+    const isLeft = launch.id % 2 === 0 ? "left" : "right"
+    return <>
+        <Fade>
+            <div className="mission" key={launch.id}>
+                <div className="mission_name" key={launch.id}>
+                    <a href={launch.links.video_link} target="_blank" rel="noreferrer"><b>Mission Name:</b> {launch.mission_name}</a>
+                    <p className="rocket_name"><b>Rocket Name:</b> {launch.rocket.rocket_name}</p>
+                    <p className="mission_date"><b>Launch Date: </b> {moment(launch.rocket.launch_date_local).format("DD MMM YYYY hh:MM")}</p>
+                    <p className="mission_place"><b>Launch Site: </b> {launch.launch_site.site_name_long}</p>
+                    { launch.ships.length ? <div className="image">
+                        <img width="256" height="256" src={launch.ships[0].image} />
+                    </div> : <></> }
+                </div>
+            </div>
+        </Fade>
+    </>
+}
+
+export default MissionCard
